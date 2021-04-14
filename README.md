dac9c5e89af6ba434d17070b66142e9f391c3dd7
Adetunji Oyebanji <adetunji.oyebanji@andela.com>
2021-04-14 02:26:05 +0100

    added options for table and column graph

---
+++ README.md
@@ -0,0 +1,4 @@
+## Starting the app
+Use the shell script to start both UI and API servers by running `./start.sh`
+
+If you want to start the servers individually cd into each root dir and run `npm run start`
--- frontend/src/containers/ngrams.js
+++ frontend/src/containers/ngrams.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Col, List, Row, Spin, Typography } from 'antd';
+import { Col, List, Row, Spin, Switch, Table, Typography } from 'antd';
 import { Column } from '@ant-design/charts';
 
 const baseurl = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000';
@@ -10,6 +10,8 @@ const Ngram = () => {
   const [ loaded, setLoaded ] = useState(false);
   const [ ngram, setNgram ] = useState([]);
   const [ ngramLoading, setNgramLoading ] = useState(false);
+  const [ displayColumn, setDisplayDisplayColumn ] = useState(true);
+
   const histogramConfig = {
     data: ngram,
     binField: 'value',
@@ -17,6 +19,19 @@ const Ngram = () => {
     xField: 'ngram',
     yField: 'count'
   };
+
+  const tableColumns = [
+    {
+      title: 'Ngram',
+      dataIndex: 'ngram',
+      key: 'ngram',
+    },
+    {
+      title: 'Count',
+      dataIndex: 'count',
+      key: 'ngram',
+    },
+  ];
 
   useEffect(() => {
     fetch(baseurl)
@@ -32,36 +47,51 @@ const Ngram = () => {
   }
 
   return (
-    <Row>
-      <Col span={8}>
-        <List
-          dataSource={data}
-          renderItem={(item) => (
-            <List.Item className="ngram-link-item" onClick={(el) => {
-              setNgramLoading(true);
-              fetch(`${baseurl}/ngram/${item.id}`)
-              .then(response => response.json())
-              .then(ngram => {
-                setNgram(ngram);
-                setNgramLoading(false);
-              });
-            }}>
-              <Typography.Text>
-                {item.body}
-              </Typography.Text>
-            </List.Item>
-          )}
-        />
-      </Col>
-      <Col span={16}>
-        {
-          ngramLoading && <Spin/>
-        }
-        {
-          !ngramLoading && <Column { ...histogramConfig } />
-        }
-      </Col>
-    </Row>
+    <>
+      <Row justify='end'>
+        <Col span={18}></Col>
+        <Col span={6}>
+          Table <Switch defaultChecked={displayColumn} onChange={(value) => {
+            setDisplayDisplayColumn(value);
+            console.log(value)
+          }} /> Histogram
+        </Col>
+      </Row>
+      <Row gutter={[20,20]}>
+        <Col span={8}>
+          <List
+            dataSource={data}
+            renderItem={(item) => (
+              <List.Item className="ngram-link-item" onClick={(el) => {
+                setNgramLoading(true);
+                fetch(`${baseurl}/ngram/${item.id}`)
+                .then(response => response.json())
+                .then(ngram => {
+                  setNgram(ngram);
+                  setNgramLoading(false);
+                });
+              }}>
+                <Typography.Text>
+                  {item.body}
+                </Typography.Text>
+              </List.Item>
+            )}
+          />
+        </Col>
+        <Col span={16}>
+          {
+            ngramLoading && <Spin/>
+          }
+          {
+            !ngramLoading && displayColumn && <Column { ...histogramConfig } />
+          }
+
+          {
+            !ngramLoading && !displayColumn && <Table dataSource={ngram} columns={tableColumns} />
+          }
+        </Col>
+      </Row>
+    </>
   )
 }
 
